68e2ce31b8cfedad637417cc82cdcccdae957b14
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-12-07 08:32:09 -0700

    changes in the routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -8,14 +8,6 @@ $routes->group('admin', function ($routes) {
                 
                     );
 
-    $routes->resource('storages', [
-        'filter' => 'permission:storages-permission',
-        'controller' => 'storagesController',
-        'except' => 'show',
-        'namespace' => 'julio101290\boilerplatetypesofmovement\Controllers',
-    ]);
-
-
     $routes->resource('tipos_movimientos_inventario', [
         'filter' => 'permission:tipos_movimientos_inventario-permission',
         'controller' => 'tipos_movimientos_inventarioController',
@@ -23,10 +15,14 @@ $routes->group('admin', function ($routes) {
         'namespace' => 'julio101290\boilerplatetypesofmovement\Controllers',
     ]);
 
-    $routes->post('tipos_movimientos_inventario/save', 'Tipos_movimientos_inventarioController::save');
-    $routes->post('tipos_movimientos_inventario/getTipos_movimientos_inventario', 'Tipos_movimientos_inventarioController::getTipos_movimientos_inventario');
-
-   
+    $routes->post('tipos_movimientos_inventario/save'
+                , 'Tipos_movimientos_inventarioController::save'
+                ,['namespace' => 'julio101290\boilerplatetypesofmovement\Controllers']
+                );
+    $routes->post('tipos_movimientos_inventario/getTipos_movimientos_inventario'
+                , 'Tipos_movimientos_inventarioController::getTipos_movimientos_inventario'
+                ,['namespace' => 'julio101290\boilerplatetypesofmovement\Controllers']
+                );
 
 
 });
