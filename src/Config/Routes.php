2f9178b1668a06441bcdbe27f77d52c01cd7adc2
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-12-09 10:11:32 -0700

    Correction errors

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -3,31 +3,26 @@
 $routes->group('admin', function ($routes) {
 
 
-
-
-
-
-    
-    $routes->resource('proveedores', [
-        'filter' => 'permission:proveedores-permission',
-        'controller' => 'ProveedoresController',
-        'except' => 'show',
-        'namespace' => 'julio101290\boilerplatesuppliers\Controllers',
-    ]);
-
-    $routes->post('proveedores/save'
-                , 'ProveedoresController::save'
-                ,['namespace' => 'julio101290\boilerplatesuppliers\Controllers']
-                );
-
-    $routes->post('proveedores/getProveedores'
-                    , 'ProveedoresController::getProveedores'
-                    ,['namespace' => 'julio101290\boilerplatesuppliers\Controllers']
+    $routes->post('tiposMovimientoInventario/getTiposMovimientoInventarioAjax'
+                    , 'Tipos_movimientos_inventarioController::getTiposMovimientoAjax'
+                    ,['namespace' => 'julio101290\boilerplatetypesmovement\Controllers']
                     );
 
-    $routes->post('proveedores/getProveedoresAjax'
-                , 'ProveedoresController::getProveedoresAjax'
-                ,['namespace' => 'julio101290\boilerplatesuppliers\Controllers']
+    $routes->resource('tipos_movimientos_inventario', [
+        'filter' => 'permission:tipos_movimientos_inventario-permission',
+        'controller' => 'tipos_movimientos_inventarioController',
+        'except' => 'show',
+        'namespace' => 'julio101290\boilerplatetypesmovement\Controllers',
+    ]);
+
+    $routes->post('tipos_movimientos_inventario/save'
+                , 'Tipos_movimientos_inventarioController::save'
+                ,['namespace' => 'julio101290\boilerplatetypesmovement\Controllers']
                 );
+    $routes->post('tipos_movimientos_inventario/getTipos_movimientos_inventario'
+                , 'Tipos_movimientos_inventarioController::getTipos_movimientos_inventario'
+                ,['namespace' => 'julio101290\boilerplatetypesmovement\Controllers']
+                );
+
 
 });
